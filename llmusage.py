b941d2f611ee1542df2c5e8ca5364cb79a243a25
NinjaHawk2014 <kanirudh541@gmail.com>
2024-11-02 17:16:53 -0400

    starting to integrate backend with frontend

--- llmusage.py
+++ llmusage.py
@@ -1,27 +1,14 @@
 import llm
-import time
 from notereader import read_image_file
 
+def notes_summary(text = None, file=None):
+    model = llm.get_model("Meta-Llama-3-8B-Instruct")
+    conversation = model.conversation()
+    if file:
+        notes_text = read_image_file(file)
+    else:
+        notes_text = text
+    response = conversation.prompt(f"Summarize {notes_text}", max_tokens = 1000)
+    return response
 
-model = llm.get_model("Meta-Llama-3-8B-Instruct")
-conversation = model.conversation()
-start_time = time.time()
-#userinput = input("What can I help with? ")
-notes_text = read_image_file("hacknjit-2024/textimagetest.png")
-print("Response recieved!")
-response = conversation.prompt(f"Summarize {notes_text}", max_tokens = 1000)
-for chunk in response.text():
-    print(chunk, end="")
-
-
-
-"""while userinput != "end":
-    response = conversation.prompt(userinput, max_tokens = 1000)
-    for chunk in response.text():
-        print(chunk, end="")
-    elapsed_time = time.time() - start_time     
-    print(f"\nElapsed time: {elapsed_time} seconds")
-    userinput = input("Message the LLM... ")
-    start_time = time.time()
-"""
 
