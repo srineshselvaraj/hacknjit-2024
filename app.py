6063be4df8853679f3b663606929641c41ba5e46
NinjaHawk2014 <kanirudh541@gmail.com>
2024-11-03 10:19:52 -0500

    sync

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def get_data():
     if request.method == "GET":
         data = notes_handler(request="summary")
     print("im here now")
-    return jsonify(data)
+    return jsonify({"summary": data})
 
 @app.route('/questions', methods=["GET", "POST"])
 def questions():
