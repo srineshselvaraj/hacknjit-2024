715a1f0e305885d7b28d7e9b43d91ebaded1a231
srineshselvaraj <srinesh9@gmail.com>
2024-11-03 00:24:44 -0400

    Added rough layout for Summary

--- client/src/Summary.js
+++ client/src/Summary.js
@@ -1,4 +1,56 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const DisplaySummary = ({summary, loading}) => {
+    return(
+        <div className="container">
+        <div className="mt-4">
+        {loading ? (
+            <div className="container">
+                <div className="align-items-center">
+                <p>Loading summary...</p>
+                </div>
+            </div>
+            ) : (
+            <p>{summary}</p>
+            )}
+        </div>
+        </div>
+    );
+}
+
+const Return = () => {
+    return (
+        <div className='container'>
+            <p>
+                Back to <Link to="/">Home</Link>
+            </p>
+        </div>
+    );
+}
+
 const Summary = () => {
-    
+    const [text, setText] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const getSummary = async() => {
+        try{
+            const response = await fetch('http://localhost:5000/get-data');
+            const data = await response.json();
+            setText(data);
+        } catch(error) {
+            console.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    getSummary();
+
+    return(
+        <div>
+          <Return />
+          <DisplaySummary summary={text} loading={loading}/>
+        </div>
+      );
 }
 export default Summary;
